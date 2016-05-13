6a25f18327407ad48862934e41a1393a47e24f3a
NPCMX <15002875182@163.com>
2016-05-13 13:42:54 +0800

    Create some databases.

---
+++ app/src/main/java/com/npcmx/justweather/app/db/JustWeatherOpenHelper.java
@@ -0,0 +1,46 @@
+package com.npcmx.justweather.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by CMX on 2016/5/13.
+ */
+public class JustWeatherOpenHelper extends SQLiteOpenHelper {
+
+    /*Province表建表语句*/
+    public static final String CREATE_PROVINCE = "create table Province("
+            + "id integer primary key autoincrement,"
+            + "province_name text,"
+            + "province_code text)";
+
+    /*City表建表语句*/
+    public static final String CREATE_CITY = "create table City("
+            + "id integer primary key autoincrement,"
+            + "city_name text,"
+            + "city_code text,"
+            + "province_id integer)";
+
+    /*County表建表语句*/
+    public static final String CREATE_COUNTY = "create table City("
+            + "id integer primary key autoincrement,"
+            + "city_name text,"
+            + "city_code text,"
+            + "city_id integer)";
+
+    public JustWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,int version){
+        super(context,name,factory,version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PROVINCE);//创建省份表
+        db.execSQL(CREATE_CITY);//创建城市表
+        db.execSQL(CREATE_COUNTY);//创建县城表
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
